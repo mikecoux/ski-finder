ba9a9928b59d2679a8aac800d07880e2f893b5d4
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-04-28 12:21:21 -0600

    icon and title

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,7 +7,7 @@ export default function Root() {
         <div id='nav-bar' className='border-b-4'>
             <nav className='flex flow-row space-x-5 text-xl pt-6 pb-6 px-8'>
                 <NavLink to='/' end>
-                    <img src='/images/profile.png' alt='Mike' className='h-12 hover:blur-sm'/>
+                    <img src='/images/profile.webp' alt='Mike' className='h-12 hover:blur-sm'/>
                 </NavLink>
                 <NavLink to='/skis' end className={'text-sky-500 hover:text-sky-700 self-center'}>
                     Browse Skis
